5549af2c58c4e4489738944c933b5956d908a20a
Jayson <jayson.martinez@devtac.ph>
2019-03-12 08:50:07 +0800

    Code Formatting

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
-import { combineReducers } from 'redux';
-import auth from './auth'
-import eventReducer from './eventReducer'
+import { combineReducers } from "redux";
+import auth from "./auth";
+import eventReducer from "./eventReducer";
 
 const rootReducer = combineReducers({
-    auth,
-    event: eventReducer
+  auth,
+  event: eventReducer
 });
- 
+
 export default rootReducer;
