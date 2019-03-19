92fc4d7d8a3b0fafe1fd7230f89dc95197317bb2
Jayson <jayson.martinez@devtac.ph>
2019-03-19 15:24:04 +0800

    Add bugsnage

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 import { AppRegistry } from 'react-native';
 import RootAppComponent from './App';
 import bgMessaging from './src/bgMessaging'
+import { Client } from 'bugsnag-react-native';
+const bugsnag = new Client("6c10791f6aba53199acc2ecd662a6125");
+
+bugsnag.notify(new Error("Test error"));
 
 AppRegistry.registerComponent('hoozin', () => RootAppComponent);
 // New task registration
