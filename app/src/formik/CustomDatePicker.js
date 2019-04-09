91626762c60c54ea695ccd8df33120e5004cce29
Jayson <jayson.martinez@devtac.ph>
2019-04-09 17:07:31 +0800

    HOOZ-56 - Edit event without deletion
    Add formik fields

---
+++ app/src/formik/CustomDatePicker.js
@@ -0,0 +1,56 @@
+import moment from "moment";
+import React from "react";
+import DatePicker from "react-native-datepicker";
+
+class CustomDatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.setFieldValue = this.setFieldValue.bind(this);
+  }
+
+  setFieldValue(date) {
+    const { field, form } = this.props;
+
+    form.setFieldValue(field.name, date);
+
+    if (field.name == "startTime" && form.values["startDate"] != "") {
+      let p = form.values["startDate"] + " " + date;
+
+      let m = moment(p, "YYYY-MM-DD h:mm A").valueOf();
+      let hour = moment(p, "YYYY-MM-DD h:mm A").format("h");
+      let amPm = moment(p, "YYYY-MM-DD h:mm A").format("A");
+      let ampm = amPm;
+
+      if (hour == 12 && amPm == "AM") {
+        ampm = "PM";
+      } else if (hour == 12 && amPm == "PM") {
+        ampm = "AM";
+      }
+
+      form.setFieldValue(
+        "endTime",
+        moment(m)
+          .add(1, "h")
+          .format("hh:mm") + ampm
+      );
+      form.setFieldValue(
+        "endDate",
+        moment(p, "YYYY-MM-DD").format("YYYY-MM-DD")
+      );
+    }
+  }
+
+  render() {
+    const { field } = this.props;
+    return (
+      <DatePicker
+        date={field.value}
+        onDateChange={this.setFieldValue}
+        {...this.props}
+      />
+    );
+  }
+}
+
+export default CustomDatePicker;
---
+++ app/src/formik/EventGooglePlaces.js
@@ -0,0 +1,61 @@
+import React from "react";
+import RNGooglePlaces from "react-native-google-places";
+import { TouchableOpacity, TextInput } from "react-native";
+
+class EventGooglePlaces extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      textInputHeight: 0
+    };
+  }
+
+  render() {
+    const { field, form } = this.props;
+
+    return (
+      <TouchableOpacity
+        style={{ height: 85, width: "100%", marginTop: -10 }}
+        onPress={() => {
+          RNGooglePlaces.openAutocompleteModal({
+            type: "address"
+          })
+            .then(place => {
+              form.setFieldValue(field.name, place.address);
+              form.setFieldValue("evtCoords", {
+                lat: place.latitude,
+                lng: place.longitude
+              });
+            })
+            .catch(e => {
+              console.log(e);
+            });
+        }}
+      >
+        <TextInput
+          ref="eventLoc"
+          selection={{ start: 0, end: 0 }}
+          multiline={true}
+          editable={false}
+          value={field.value}
+          onContentSizeChange={event => {
+            this.setState({
+              textInputHeight: event.nativeEvent.contentSize.height
+            });
+          }}
+          style={{
+            marginTop: 35,
+            marginLeft: -5,
+            fontSize: 17,
+            color: "#707070",
+            height: Math.max(45, this.state.textInputHeight)
+          }}
+          placeholder="Location"
+        />
+      </TouchableOpacity>
+    );
+  }
+}
+
+export default EventGooglePlaces;
